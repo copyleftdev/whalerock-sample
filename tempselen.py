7aaa0c90b6d0b2a8283c1922c39589b63974a07e
Don Johnson <copyleftdev@gmail.com>
2015-10-24 14:46:23 -0700

    deleted sample scratch file

--- tempselen.py
+++
@@ -1,7 +0,0 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-
-
-driver = webdriver.Firefox()
-driver.get("http://www.whalerockindustries.com/")
-driver.find_element(By.linkText("View Profile")).click()
